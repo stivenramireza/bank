6efc9317745b6280de49b3e6d8128d1b7fce8c20
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-13 13:33:23 -0500

    Adding new test cases

--- customer-microservice/src/test/java/com/stivenramireza/bank/repository/CustomerRepositoryIT.java
+++ customer-microservice/src/test/java/com/stivenramireza/bank/repository/CustomerRepositoryIT.java
@@ -2,6 +2,9 @@ package com.stivenramireza.bank.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -58,6 +61,61 @@ class CustomerRepositoryIT {
 		
 		// Assert
 		assertNotNull(customer, "The customer is null, cannot be saved");
+	}
+	
+	@Test
+	@Order(3)
+	void shouldUpdateCustomer() {
+		// Arrange
+		Integer customerId = 14836554;
+		Customer customer = null;
+		
+		customer = customerRepository.findById(customerId).get();
+		customer.setEnable("N");
+		
+		// Act
+		customer = customerRepository.save(customer);
+		
+		// Assert
+		assertNotNull(customer, "The customer is null, cannot be updated");
+		assertEquals(14836554, customer.getCustId());
+		assertEquals("N", customer.getEnable());
+		
+	}
+	
+	@Test
+	@Order(4)
+	void shouldDeleteCustomer() {
+		// Arrange
+		Integer customerId = 14836554;
+		Customer customer = null;
+		Optional<Customer> optionalCustomer = null;
+		
+		Boolean existsCustomer = customerRepository.findById(customerId).isPresent();
+		assertTrue(existsCustomer, "The customer was not found");
+		
+		customer = customerRepository.findById(customerId).get();
+		
+		// Act
+		customerRepository.delete(customer);
+		optionalCustomer = customerRepository.findById(customerId);
+		
+		// Assert
+		assertFalse(optionalCustomer.isPresent(), "The customer could not be deleted");
+	}
+	
+	@Test
+	@Order(5)
+	void shouldGetAllCustomers() {
+		// Arrange
+		List<Customer> customers = null;
+		
+		// Act
+		customers = customerRepository.findAll();
+		customers.forEach(customer -> log.info(customer.getName()));
+		
+		// Assert
+		assertFalse(customers.isEmpty(), "Customers do not exist");
 	}
 
 }
