61336cf3caf5b9abfddf7c7d95120708b9f3de01
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-17 20:08:58 -0500

    User microservice

---
+++ user-microservice/src/main/java/com/stivenramireza/bank/UserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stivenramireza.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+}
