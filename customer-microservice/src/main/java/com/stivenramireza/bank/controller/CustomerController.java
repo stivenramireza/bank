0be86783e6747a84b8c56eb1048a109697514189
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-18 18:56:46 -0500

    Customer Controller

--- customer-microservice/src/main/java/com/stivenramireza/bank/controller/CustomerController.java
+++ customer-microservice/src/main/java/com/stivenramireza/bank/controller/CustomerController.java
@@ -1,37 +1,16 @@
 package com.stivenramireza.bank.controller;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.stivenramireza.bank.domain.Customer;
-import com.stivenramireza.bank.dto.CustomerDTO;
-import com.stivenramireza.bank.mapper.CustomerMapper;
 import com.stivenramireza.bank.service.CustomerService;
 
 @RestController
 @RequestMapping("/api/v1/customers")
-@CrossOrigin(origins = "*")
 public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
-	
-	@Autowired
-	private CustomerMapper customerMapper;
-
-	
-	@GetMapping
-	public ResponseEntity<List<CustomerDTO>> findAll() throws Exception {
-		List<Customer> foundCustomers = customerService.findAll();
-		List<CustomerDTO> customers = customerMapper.listCustomerToListCustomerDTO(foundCustomers);
-		return ResponseEntity.ok(customers);
-	}
 
 }
