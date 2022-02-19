d2cc314e4996ecedc528ff322dfd07170b8eb3e0
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-18 19:00:09 -0500

    Customer controller updated with complex domain

--- customer-microservice/src/main/java/com/stivenramireza/bank/controller/CustomerController.java
+++ customer-microservice/src/main/java/com/stivenramireza/bank/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.stivenramireza.bank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stivenramireza.bank.domain.Customer;
 import com.stivenramireza.bank.service.CustomerService;
 
 @RestController
@@ -12,5 +16,10 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
+
+	@GetMapping
+	public List<Customer> findAll() throws Exception {
+		return null;
+	}
 
 }
